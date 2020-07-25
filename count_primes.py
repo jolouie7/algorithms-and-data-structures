7f4827b868fd1c2ab45b001c4e881c65caca3e9b
Your_Name <joseph161216@gmail.com>
2020-07-25 00:10:40 -0700

    add best time to buy and sell stock 2

--- count_primes.py
+++ count_primes.py
@@ -9,8 +9,8 @@
 # Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 
 """
-Time Complexity: o(n)
-Space Complexity: o(n)
+Time Complexity: O(n)
+Space Complexity: O(n)
 """
 
 class Solution:
