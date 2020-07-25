0406cf76517d2923a5cf3afd0959cec80ce31613
Your_Name <joseph161216@gmail.com>
2020-07-24 23:28:04 -0700

    add time and space complexity

--- count_primes.py
+++ count_primes.py
@@ -8,6 +8,10 @@
 # Output: 4
 # Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 
+"""
+Time Complexity: o(n)
+Space Complexity: o(n)
+"""
 
 class Solution:
     def countPrimes(self, n: int) -> int:
