f1c7b087a353ebff900be8a2942ac87f81d97341
Your_Name <joseph161216@gmail.com>
2020-12-20 00:00:35 -0800

    Add contains duplicates

---
+++ Problems/Leetcode/217_Contains_Duplicate.py
@@ -0,0 +1,31 @@
+# https: // leetcode.com/problems/contains-duplicate/
+# 217. Contains Duplicate
+
+# Given an array of integers, find if the array contains any duplicates.
+# Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+# Example 1:
+# Input: [1, 2, 3, 1]
+# Output: true
+
+# Example 2:
+# Input: [1, 2, 3, 4]
+# Output: false
+
+# Example 3:
+# Input: [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+# Output: true
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # use a set
+        # if we see a number already in the set we return true
+        seen = set()
+        for ele in nums:
+            if ele in seen:
+                return True
+            else:
+                seen.add(ele)
+        # return false when we finish going through entire list
+        return False
