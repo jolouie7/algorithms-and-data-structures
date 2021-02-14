081b2b8a3f1952c9ba2cc3c0eda546c794b66844
Your_Name <joseph161216@gmail.com>
2021-02-13 23:03:25 -0800

    Add Same Tree

---
+++ Problems/Leetcode/100_Same_Tree.py
@@ -0,0 +1,24 @@
+# 100. Same Tree
+# https://leetcode.com/problems/same-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p is None and q is None:
+            return True
+        elif p is None or q is None:
+            return False
+        elif p.val != q.val:
+            return False
+        else:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+# Time: o(n), we are touching every tree node
+# Space: o(n), we use recursion
